094039f45a45791909017df24fd66a7665cde907
zihe <hezihao@linghit.com>
2019-11-18 21:26:00 +0800

    支持自定义属性配置，弧线颜色、弧线大小等。

--- app/src/main/java/com/hzh/circle/progress/sample/widget/CircleProgressWithTextView.java
+++ app/src/main/java/com/hzh/circle/progress/sample/widget/CircleProgressWithTextView.java
@@ -1,6 +1,10 @@
 package com.hzh.circle.progress.sample.widget;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,6 +13,9 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import com.hzh.circle.progress.sample.R;
 
 /**
  * Package: com.hzh.circle.progress.sample.widget
@@ -18,8 +25,38 @@ import android.view.View;
  * Descirbe:
  * Email: hezihao@linghit.com
  */
-
 public class CircleProgressWithTextView extends View {
+    /**
+     * 默认值
+     */
+    /**
+     * 默认的当前进度，默认为0
+     */
+    private static final int DEFAULT_PROGRESS = 0;
+    /**
+     * 默认的最大值，默认为100
+     */
+    private static final int DEFAULT_MAX = 100;
+    /**
+     * 默认进度圆弧颜色
+     */
+    private final int DEFAULT_CIRCLE_COLOR = Color.parseColor("#0AA4A2");
+    /**
+     * 圆弧颜色
+     */
+    private final int DEFAULT_REMAIN_CIRCLE_COLOR = Color.parseColor("#EFEFF0");
+    /**
+     * 默认背景颜色
+     */
+    private final int DEFAULT_BG_COLOR = Color.parseColor("#00000000");
+    /**
+     * 默认文字颜色
+     */
+    private final int DEFAULT_TEXT_COLOR = Color.parseColor("#0AA4A2");
+    /**
+     * 是否显示进度百分比文字
+     */
+    private static final boolean DEFAULT_TEXT_ENABLE = true;
     /**
      * 绘制相关
      */
@@ -30,9 +67,14 @@ public class CircleProgressWithTextView extends View {
     /**
      * 画笔颜色
      */
-    private int mCircleColor = Color.parseColor("#0AA4A2");
-    private int mRemainCircleColor = Color.parseColor("#EFEFF0");
-    private int mTextColor = Color.parseColor("#0AA4A2");
+    private int mCircleColor;
+    private int mRemainCircleColor;
+    private int mTextColor;
+    private int mBgColor;
+    /**
+     * 圆弧宽度
+     */
+    private float mCircleBorderWidth;
     /**
      * View相关尺寸
      */
@@ -49,7 +91,11 @@ public class CircleProgressWithTextView extends View {
     /**
      * 进度最大值
      */
-    private float mMax = 100;
+    private int mMax;
+    /**
+     * 是否显示百分比文字
+     */
+    private boolean mEnableText;
     /**
      * 弧线的开始角度，默认是0，是水平的，我们要从上面开始画
      */
@@ -59,27 +105,68 @@ public class CircleProgressWithTextView extends View {
      */
     private int mCenterX;
     private int mCenterY;
+    /**
+     * 进度条进度
+     */
+    private ValueAnimator mProgressAnimator;
+    private OnProgressUpdateListener mProgressUpdateListener;
 
     public CircleProgressWithTextView(Context context) {
         super(context);
-        init();
+        init(null);
     }
 
     public CircleProgressWithTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(attrs);
     }
 
     public CircleProgressWithTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(attrs);
     }
 
-    private void init() {
+    /**
+     * 初始化自定义属性
+     */
+    private void initAttributeVar(AttributeSet attrs) {
+        //默认圆弧宽度
+        int defaultCircleBorderWidth = dip2px(getContext(), 5f);
+        if (attrs != null) {
+            TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.CircleProgressWithTextView);
+            mProgress = array.getInt(R.styleable.CircleProgressWithTextView_cpt_progress, DEFAULT_PROGRESS);
+            mMax = array.getInt(R.styleable.CircleProgressWithTextView_cpt_max, DEFAULT_MAX);
+            //Xml设置的进度圆弧颜色
+            mCircleColor = array.getColor(R.styleable.CircleProgressWithTextView_cpt_circle_color, DEFAULT_CIRCLE_COLOR);
+            //Xml设置的圆弧颜色
+            mRemainCircleColor = array.getColor(R.styleable.CircleProgressWithTextView_cpt_remain_circle_color, DEFAULT_REMAIN_CIRCLE_COLOR);
+            //文字颜色
+            mTextColor = array.getColor(R.styleable.CircleProgressWithTextView_cpt_text_color, DEFAULT_CIRCLE_COLOR);
+            //读取设置的圆弧轮廓宽度，读取dimension
+            mCircleBorderWidth = array.getDimensionPixelSize(R.styleable.CircleProgressWithTextView_cpt_remain_circle_border_width, defaultCircleBorderWidth);
+            //是否显示进度百分比文字
+            mEnableText = array.getBoolean(R.styleable.CircleProgressWithTextView_cpt_text_enable, DEFAULT_TEXT_ENABLE);
+            //背景颜色
+            mBgColor = array.getColor(R.styleable.CircleProgressWithTextView_cpt_bg_color, DEFAULT_BG_COLOR);
+            array.recycle();
+        } else {
+            //没有在Xml中设置属性，使用默认属性
+            mProgress = DEFAULT_PROGRESS;
+            mMax = DEFAULT_MAX;
+            mCircleColor = DEFAULT_CIRCLE_COLOR;
+            mRemainCircleColor = DEFAULT_REMAIN_CIRCLE_COLOR;
+            mTextColor = DEFAULT_TEXT_COLOR;
+            mCircleBorderWidth = defaultCircleBorderWidth;
+            mEnableText = DEFAULT_TEXT_ENABLE;
+        }
+    }
+
+    private void init(AttributeSet attrs) {
+        initAttributeVar(attrs);
         //外圆画笔
         mCirclePaint = new Paint();
         mCirclePaint.setColor(mCircleColor);
-        mCirclePaint.setStrokeWidth(dip2px(getContext(), 5f));
+        mCirclePaint.setStrokeWidth(mCircleBorderWidth);
         mCirclePaint.setStyle(Paint.Style.STROKE);
         //设置笔触为圆角
         mCirclePaint.setStrokeCap(Paint.Cap.ROUND);
@@ -152,20 +239,45 @@ public class CircleProgressWithTextView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.scale(0.98f, 0.98f, mCenterX, mCenterY);
+        canvas.scale(0.93f, 0.93f, mCenterX, mCenterY);
+        drawBg(canvas);
+        float curProgress = getProgress();
+        //画圆弧
+        drawCircle(canvas, curProgress);
+        if (mEnableText) {
+            //画文字
+            String progressText = String.valueOf((int) curProgress);
+            drawProgressText(canvas, progressText);
+        }
+    }
+
+    /**
+     * 画背景
+     */
+    private void drawBg(Canvas canvas) {
+        canvas.drawColor(mBgColor);
+    }
+
+    /**
+     * 画圆弧
+     */
+    private void drawCircle(Canvas canvas, float curProgress) {
         mCirclePaint.setColor(mRemainCircleColor);
         canvas.drawCircle(mCenterX, mCenterY, mRadius, mCirclePaint);
         //绘制当前进度的弧线
         mCirclePaint.setColor(mCircleColor);
-        float curProgress = getProgress();
         float angle = 360 * (curProgress * 1.0f / getMax());
         canvas.drawArc(mRect, mStartAngle, angle, false, mCirclePaint);
         //绘制剩下的度数的弧线
         //float remainAngle = 360f - angle;
         //mCirclePaint.setColor(mRemainCircleColor);
         //canvas.drawArc(mRect, mStartAngle + angle, remainAngle, false, mCirclePaint);
-        //画文字
-        String progressText = String.valueOf((int) curProgress);
+    }
+
+    /**
+     * 画进度文字
+     */
+    private void drawProgressText(Canvas canvas, String progressText) {
         Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
         float baseLine = -(fontMetrics.ascent + fontMetrics.descent) / 2;
         float textWidth = mTextPaint.measureText(progressText);
@@ -174,8 +286,6 @@ public class CircleProgressWithTextView extends View {
         canvas.drawText(progressText, startX, endY, mTextPaint);
         //画百分比
         String percentText = "%";
-        //百分比文字的宽度
-        //float percentTextWidth = mTextPaint.measureText(percentText);
         //计算百分比文字的起始X坐标
         float percentStartX = 0;
         if (progressText.length() == 1) {
@@ -188,11 +298,90 @@ public class CircleProgressWithTextView extends View {
         canvas.drawText(percentText, percentStartX, percentEndY, mPercentPaint);
     }
 
+    /**
+     * 指定时间，开始进度
+     *
+     * @param preProgress 之前的进度
+     * @param duration    执行时间
+     */
+    public void startProgressByTime(int preProgress, long duration) {
+        if (mProgressAnimator == null) {
+            mProgressAnimator = ValueAnimator.ofInt(preProgress, mMax);
+        }
+        mProgressAnimator.setInterpolator(new LinearInterpolator());
+        mProgressAnimator.setDuration(duration);
+        mProgressAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Integer cValue = (Integer) animation.getAnimatedValue();
+                setProgress(cValue);
+                if (mProgressUpdateListener != null) {
+                    mProgressUpdateListener.onProgressUpdate(cValue);
+                }
+            }
+        });
+        mProgressAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                if (mProgressUpdateListener != null) {
+                    mProgressUpdateListener.onStart();
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                //结束，设置回0
+                setProgress(0);
+                if (mProgressUpdateListener != null) {
+                    mProgressUpdateListener.onEnd();
+                }
+            }
+        });
+        mProgressAnimator.start();
+    }
+
+    /**
+     * 进度更新监听
+     */
+    public interface OnProgressUpdateListener {
+        void onStart();
+
+        /**
+         * 进度更新
+         *
+         * @param curProgress 当前进度
+         */
+        void onProgressUpdate(int curProgress);
+
+        void onEnd();
+    }
+
+    public static class OnProgressUpdateAdapter implements OnProgressUpdateListener {
+
+        @Override
+        public void onStart() {
+        }
+
+        @Override
+        public void onProgressUpdate(int curProgress) {
+        }
+
+        @Override
+        public void onEnd() {
+        }
+    }
+
+    public void setOnProgressUpdateListener(OnProgressUpdateListener progressUpdateListener) {
+        mProgressUpdateListener = progressUpdateListener;
+    }
+
     public float getProgress() {
         return mProgress;
     }
 
-    public synchronized void setProgress(float mProgress) {
+    public void setProgress(float mProgress) {
         this.mProgress = mProgress;
         postInvalidate();
     }
@@ -201,8 +390,8 @@ public class CircleProgressWithTextView extends View {
         return mMax;
     }
 
-    public synchronized void setMax(float mMax) {
-        this.mMax = mMax;
+    public void setMax(int max) {
+        this.mMax = max;
         postInvalidate();
     }
 
